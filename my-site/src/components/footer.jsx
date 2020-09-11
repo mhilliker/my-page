db76c8a30ad012c2e177422cda81dc3552805363
mhilliker <mrhilliker@gmail.com>
2020-09-11 18:57:44 -0500

    Create footer.jsx

---
+++ my-site/src/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Footer extends React.Component {
+	render() {
+		return (
+			<div id="footer" className="text-center">
+				<div className="container">
+					<h5 className="">Mark Hilliker &copy; 2020</h5>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Footer;
