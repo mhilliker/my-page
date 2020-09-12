af3cb2eaff844706b77fc528285952887f13643d
mhilliker <mrhilliker@gmail.com>
2020-09-12 09:20:24 -0500

    add profile picture and fix padding issue

--- my-site/src/App.js
+++ my-site/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/navbar';
@@ -12,18 +11,6 @@ function App() {
     <div className="App">
       <Navbar />
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         <About />
         <Education />
       </header>
--- my-site/src/components/about.jsx
+++ my-site/src/components/about.jsx
@@ -3,6 +3,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 
 import FullscreenItem from './fullscreenitem';
+import profilePicture from '../assets/images/profile.PNG';
 
 class About extends React.Component {
     render() {
@@ -11,7 +12,7 @@ class About extends React.Component {
                 <div className="aboutPage fullscreenContent">
                    <Jumbotron>
                     <h1>Hello, I am Mark Hilliker!</h1>
-                    <div>image here</div>
+                    <img src={profilePicture} alt="profile" className="profilePic" />
                     <p>
                     I am a software engineer in the healthcare tech industry in Madison, Wisconsin.
                     My professional interests include full stack web development, machine learning, and data analytics.
