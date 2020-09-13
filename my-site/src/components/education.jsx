291b69d1ebdf0f76ce258374e405d9ad7e778f41
mhilliker <mrhilliker@gmail.com>
2020-09-12 19:26:16 -0500

    add education info
    still need to update course lists

--- my-site/src/components/education.jsx
+++ my-site/src/components/education.jsx
@@ -3,6 +3,9 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import FullscreenItem from './fullscreenitem';
 
@@ -11,9 +14,9 @@ class Education extends React.Component {
         return (
             <FullscreenItem id="educationPage">
                 <div className="educationPage fullscreenContent">
-                <Jumbotron>
-                    <h1>Education</h1>
-                    <EducationAccordion />
+                    <Jumbotron>
+                        <h1>Education</h1>
+                        <EducationAccordion />
                     </Jumbotron>
                 </div >
             </FullscreenItem >
@@ -31,7 +34,11 @@ function EducationAccordion() {
                                 </Accordion.Toggle>
                 </Card.Header>
                 <Accordion.Collapse eventKey="0">
-                    <Card.Body>Masters in Computer Science at Arizona State University</Card.Body>
+                    <Card.Body>
+                        <EducationInfoGroup degree="Masters in Computer Science" date="2018 - 2020" school="Arizona State University" gpa="4.0"
+                            courses={["Maching Learning", "Data Mining", "Mobile Computing", "Data Visualization", "Distributed Systems", 
+                            "Deep Learning in Visual Computing", "Artificial Intelligence", "Software Testing"]} />
+                    </Card.Body>
                 </Accordion.Collapse>
             </Card>
             <Card>
@@ -41,7 +48,11 @@ function EducationAccordion() {
                                 </Accordion.Toggle>
                 </Card.Header>
                 <Accordion.Collapse eventKey="1">
-                    <Card.Body>Bachelors of Science in Computer Science at ASU</Card.Body>
+                    <Card.Body>
+                    <EducationInfoGroup degree="BS in Computer Science" date="2013 - 2016" school="Arizona State University" gpa="3.73"
+                            courses={["Maching Learning", "Data Mining", "Mobile Computing", "Data Visualization", "Distributed Systems", 
+                            "Deep Learning in Visual Computing", "Artificial Intelligence", "Software Testing"]} />
+                    </Card.Body>
                 </Accordion.Collapse>
             </Card>
             <Card>
@@ -51,11 +62,42 @@ function EducationAccordion() {
                                 </Accordion.Toggle>
                 </Card.Header>
                 <Accordion.Collapse eventKey="2">
-                    <Card.Body>Bachelors of Arts in Mathematics at ASU</Card.Body>
+                    <Card.Body>
+                    <EducationInfoGroup degree="BA in Mathematics" date="2013 - 2016" school="Arizona State University" gpa="3.73"
+                            courses={["Maching Learning", "Data Mining", "Mobile Computing", "Data Visualization", "Distributed Systems", 
+                            "Deep Learning in Visual Computing", "Artificial Intelligence", "Software Testing"]} />
+                    </Card.Body>
                 </Accordion.Collapse>
             </Card>
         </Accordion>
 
+    );
+}
+
+function EducationInfoGroup(props) {
+
+    const listItems = [];
+    for(const course of props.courses){
+        listItems.push(<li>{course}</li>);
+    }
+
+    return (
+        <Container className="educationInfoGroup">
+            <Row className="titleRow">
+                <Col sm={8}>{props.degree}</Col>
+                <Col sm={4}>{props.date}</Col>
+            </Row>
+            <Row className="schoolRow">
+                <Col sm={8}>{props.school}</Col>
+                <Col sm={4}>{props.gpa} GPA</Col>
+            </Row>
+            <Row>
+                <Col className="courseRow">
+                    <p>Relevant Courses:</p>
+                    <ul className="courseList">{listItems}</ul>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
